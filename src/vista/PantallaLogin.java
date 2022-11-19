68203d07b591595987f422f786c8b85649a36898
lucashmunoz <lucashmunoz@gmail.com>
2022-11-19 01:24:13 -0300

    Added enter listener to login btn and removed unnecessary handler declaration

--- src/vista/PantallaLogin.java
+++ src/vista/PantallaLogin.java
@@ -5,6 +5,9 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,83 +20,96 @@ import javax.swing.JTextField;
 import controlador.ControladorPantalla;
 import controlador.ControladorSistema;
 
+public class PantallaLogin extends JFrame {
 
-public class PantallaLogin extends JFrame{
-	
 	private static final long serialVersionUID = 1L;
 	private JTextField usernameField, passwordField;
 	private ControladorPantalla controladorPantalla;
 	private ControladorSistema controladorSistema;
-	
-	public PantallaLogin(){
-		
+
+	public PantallaLogin() {
+
 		super("Ingresar al sistema");
 		this.setLayout(new BorderLayout());
-		
-		controladorSistema = ControladorSistema.getInstance(); 
+
+		controladorSistema = ControladorSistema.getInstance();
 		controladorPantalla = ControladorPantalla.getInstance();
-		
+
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
-		
+
 		Container usernameContainer = new Container();
 		usernameContainer.setBounds(50, 10, 200, 30);
-		usernameContainer.setLayout(new GridLayout(1,2,2,2));
+		usernameContainer.setLayout(new GridLayout(1, 2, 2, 2));
 		JLabel usernameLabel = new JLabel("Usuario:");
 		usernameField = new JTextField();
 		usernameContainer.add(usernameLabel);
 		usernameContainer.add(usernameField);
 		panel.add(usernameContainer);
-		
+
 		Container passwordContainer = new Container();
 		passwordContainer.setBounds(50, 50, 200, 30);
-		passwordContainer.setLayout(new GridLayout(1,2,2,2));
+		passwordContainer.setLayout(new GridLayout(1, 2, 2, 2));
 		JLabel passwordLabel = new JLabel("Password:");
 		passwordField = new JPasswordField(8);
 		passwordContainer.add(passwordLabel);
 		passwordContainer.add(passwordField);
 		panel.add(passwordContainer);
-		
-		
+
 		Container buttonContainer = new Container();
-		buttonContainer.setLayout(new GridLayout(1,2,2,2));
-		buttonContainer.setBounds(50,200,200,30);
-		
+		buttonContainer.setLayout(new GridLayout(1, 2, 2, 2));
+		buttonContainer.setBounds(50, 200, 200, 30);
+
 		JButton btnAccess = new JButton("Acceder");
-		
 
-		class HandlerBtnAceptar implements ActionListener{
-
+		class HandlerBtnAceptar implements ActionListener {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(controladorSistema.usuarioExiste(usernameField.getText(), passwordField.getText())) {
+				if (controladorSistema.usuarioExiste(usernameField.getText(), passwordField.getText())) {
 					System.out.println("El usuario " + usernameField.getText() + " ha ingresado al sistema.");
 					cerrarVentana();
 					controladorPantalla.mostrarPantallaInstalaciones();
-					
-				}else {
+
+				} else {
 					System.out.println("Las credenciales ingresadas son incorrectas");
 					JOptionPane.showMessageDialog(btnAccess, "Las credenciales ingresadas son incorrectas");
 				}
-				
+
 			}
 		}
-		
-		HandlerBtnAceptar btnAccessHandler = new HandlerBtnAceptar();
-		
-		btnAccess.addActionListener(btnAccessHandler);
-		
+
+		btnAccess.addActionListener(new HandlerBtnAceptar());
+		btnAccess.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					btnAccess.doClick();
+				}
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 		buttonContainer.add(btnAccess);
-		
+
 		panel.add(buttonContainer);
-		
-		this.add(panel,BorderLayout.CENTER);
-		
-		
+
+		this.add(panel, BorderLayout.CENTER);
+
 	}
-	
+
 	private void cerrarVentana() {
 		controladorPantalla.cerrarPantallaLogin();
 	}
-	
+
 }
