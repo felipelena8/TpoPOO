97b85a33571a54f78877d5f060c4dda1e9fc183b
lucashmunoz <lucashmunoz@gmail.com>
2022-11-28 23:39:36 -0300

    rollback seniority

--- src/modelo/enums/Seniority.java
+++ src/modelo/enums/Seniority.java
@@ -2,30 +2,6 @@ package modelo.enums;
 
 public enum Seniority {
 
-	JUNIOR(4, 100), SEMISENIOR(3, 70), SENIOR(2, 50);
-
-	private double tiempoParaInstalacion;
-	private double precioHora;
-
-	private Seniority(double tiempoParaInstalacion, double precioHora) {
-		this.tiempoParaInstalacion = tiempoParaInstalacion;
-		this.precioHora = precioHora;
-	}
-
-	public double getTiempoParaInstalacion() {
-		return tiempoParaInstalacion;
-	}
-
-	public void setTiempoParaInstalacion(double tiempoParaInstalacion) {
-		this.tiempoParaInstalacion = tiempoParaInstalacion;
-	}
-
-	public double getPrecioHora() {
-		return precioHora;
-	}
-
-	public void setPrecioHora(double precioHora) {
-		this.precioHora = precioHora;
-	}
+	JUNIOR, SEMISENIOR, SENIOR;
 
 }
